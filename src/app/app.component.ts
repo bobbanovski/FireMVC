a8b6cc27d719d5d4e85d15c3ff843e3967b91369
DevOps\Developer <bobbanovski@gmail.com>
2017-02-07 14:11:34 +1100

    Basic CRUD operations with firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { AngularFire } from 'angularfire2'; // must be injected to constructor
+import { AngularFire, FirebaseListObservable } from 'angularfire2'; // must be injected to constructor - fb - explicitly set type
 import { Observable } from 'rxjs/Observable';
 
 @Component({
@@ -9,17 +9,48 @@ import { Observable } from 'rxjs/Observable';
 })
 export class AppComponent {
   title = 'app works!';
-  cuisines; //= ['c1', 'c2', 'c3']
-  //private subscription;
+  //render a list
+  cuisines: FirebaseListObservable<any[]>; //= ['c1', 'c2', 'c3']
+  //render an object
+  restaurant;
+  restaurantAddress;
 
   constructor(private af: AngularFire){
     //console.log(af);
     
   }
   ngOnInit(){
-    this.cuisines = this.af.database.list('/cuisines'); //node of database - now a firebase list observable      
+    this.cuisines = this.af.database.list('/cuisines'); //node of database - now a firebase list observable    
+    this.restaurant = this.af.database.object('/restaurant');
+    console.log(this.restaurant);
   }
   // ngOnDestroy(){ // dont need this if using | async in html
   //   this.subscription.unsubscribe();
   // }
+  add(){
+    this.cuisines.push({
+      name: 'Mexican',
+      details: {
+        description: '...'
+      }
+    })
+  }
+  update(){
+    // this.af.database.object('/restaurant').update({ // nondestructive update
+    //   name: 'New Name',
+    //   rating: 5
+    // })
+    this.af.database.object('/restaurant').set({ // overrides object at this point
+      name: 'New Name',
+      rating: 6
+    })
+    //this.af.database.object('/favorites/1/10').set(true)
+    //this.af.database.object('/favorites/1/10').set(null) // deletes the above entry
+    // can be used to delete specific entries
+  }
+  remove(){
+    this.af.database.object('restaurant').remove() //promise, notifies when completed
+      .then(x => console.log("Successfully deleted"))
+      .catch(error => console.log("error in item deletion", error))
+  }  
 }
