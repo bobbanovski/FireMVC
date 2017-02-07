112890055d958aa5a23d25227a960b7b1760e939
DevOps\Developer <bobbanovski@gmail.com>
2017-02-07 11:38:08 +1100

    Link web app to firebase db
    including async piping of data to view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AngularFire } from 'angularfire2'; // must be injected to constructor
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  cuisines; //= ['c1', 'c2', 'c3']
+  //private subscription;
+
+  constructor(private af: AngularFire){
+    //console.log(af);
+    
+  }
+  ngOnInit(){
+    this.cuisines = this.af.database.list('/cuisines'); //node of database - now a firebase list observable      
+  }
+  // ngOnDestroy(){ // dont need this if using | async in html
+  //   this.subscription.unsubscribe();
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyAFhqS3kQFn0HFzYbmFgIcE_oRPd76v31U",
+    authDomain: "firemvc.firebaseapp.com",
+    databaseURL: "https://firemvc.firebaseio.com",
+    storageBucket: "firemvc.appspot.com",
+    messagingSenderId: "314145306498"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +21,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
